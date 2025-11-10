8c83502ab66c27753ee908cf07712955cb2842dd
Thibaud-Danede <151736283+Thibaud-Danede@users.noreply.github.com>
2025-11-10 13:50:05 +0100

    Génération automatique de tags (json pas encore supprimés)

---
+++ src/main/java/net/ravadael/firstuselessmod/datagen/ModBlockStateProvider.java
@@ -0,0 +1,28 @@
+package net.ravadael.firstuselessmod.datagen;
+
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.ravadael.firstuselessmod.FirstUselessMod;
+import net.ravadael.firstuselessmod.block.ModBlocks;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, FirstUselessMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.RUBY_BLOCK);
+        blockWithItem(ModBlocks.RAW_RUBY);
+        blockWithItem(ModBlocks.SOUND_BLOCK);
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
